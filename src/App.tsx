0c94e4afc3544a262295dccea570b031c944ab23
ArtLevel <algitIT@rambler.ru>
2023-09-06 12:22:49 +0300

    Exam complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './App.css'
 import { CounterOne } from './components/Counters/CounterOne'
-import { CounterTwo } from './components/Counters/CounterTwo'
 
 const App = () => {
 	return (
 		<div>
 			<CounterOne />
-			<CounterTwo />
+			{/*<CounterTwo />*/}
 		</div>
 	)
 }
