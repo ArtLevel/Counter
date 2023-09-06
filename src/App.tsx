d6f9c4e86ec6fc69adf7a87de8dbfc1f74b4e037
ArtLevel <algitIT@rambler.ru>
2023-09-06 11:30:53 +0300

    Refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,31 @@ import React, {useEffect, useState} from 'react';
 
 import {SetBlock} from './components/SetBlock';
 import {IncBlock} from './components/IncBlock';
-import './App.css';
 import {Button} from './components/Button';
+import './App.css';
 
 function App() {
 	const [score, setScore] = useState(0)
 	const [maxValue, setMaxValue] = useState(5)
 	const [minValue, setMinValue] = useState(0)
 	const [show, setShow] = useState(false)
+	console.log(score)
 
 	useEffect(() => {
 		const maxValue = localStorage.getItem('maxValue')
 		const minValue = localStorage.getItem('minValue')
-		if (maxValue) {
-			setMaxValue(parseInt(maxValue))
-		}
-		if (minValue) {
-			setMinValue(parseInt(minValue))
-			setScore(parseInt(minValue))
-		}
-	}, [])
+		const score = localStorage.getItem('score')
+
+		if(score) setScore(Number(score))
+		if (maxValue) setMaxValue(Number(maxValue))
+		if (minValue) setMinValue(Number(minValue))
+	}, []);
+
+	useEffect(() => {
+			localStorage.setItem('maxValue', `${maxValue}`)
+			localStorage.setItem('minValue', `${minValue}`)
+			localStorage.setItem('score', `${score}`)
+	}, [minValue, maxValue, score])
 
 	const incScore = () => {
 		if (score < maxValue) setScore(prevState => prevState + 1)
@@ -29,11 +34,9 @@ function App() {
 
 	const removeScore = () => setScore(minValue)
 	const onChangeMaxValue = (value: number) => {
-		localStorage.setItem('maxValue', `${value}`)
 		setMaxValue(value)
 	}
 	const onChangeMinValue = (value: number) => {
-		localStorage.setItem('minValue', `${value}`)
 		setMinValue(value)
 		setScore(value)
 	}
