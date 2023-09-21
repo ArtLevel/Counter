bacb797ea2bd400d33b85d1354e579200a183bf9
ArtLevel <algitIT@rambler.ru>
2023-09-21 12:12:21 +0300

    Fixed bugs

--- src/components/Counters/CounterOne.tsx
+++ src/components/Counters/CounterOne.tsx
@@ -2,23 +2,28 @@ import { SetBlock } from '../common/SetBlock'
 import { IncBlock } from '../common/IncBlock'
 import { Button } from '../common/Button'
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppRootStoreType } from '../../store/store'
+import { CounterValuesT, IncrementCounterAC, RemoveScoreCounterAC } from '../../store/reducers/counterValueReducer'
 
 export const CounterOne = () => {
-	const [score, setScore] = useState(0)
+	const dispatch = useDispatch()
+	const counterValues = useSelector<AppRootStoreType, CounterValuesT>(s => s.counterValue)
+
 	const [maxValue, setMaxValue] = useState(5)
 	const [minValue, setMinValue] = useState(0)
 	const [show, setShow] = useState(false)
 
 	const incScore = () => {
-		if (score < maxValue) setScore(prevState => prevState + 1)
+		if (counterValues.score < maxValue) dispatch(IncrementCounterAC())
 	}
 
-	const removeScore = () => setScore(minValue)
+	const removeScore = () => dispatch(RemoveScoreCounterAC(minValue))
 	const onChangeMaxValue = (value: number) => setMaxValue(value)
 
 	const onChangeMinValue = (value: number) => {
 		setMinValue(value)
-		setScore(value)
+		dispatch(RemoveScoreCounterAC(value))
 	}
 
 	const onShowHandler = () => setShow(prevState => !prevState)
@@ -31,7 +36,7 @@ export const CounterOne = () => {
 						<SetBlock maxValue={maxValue} minValue={minValue} onChangeMaxValue={onChangeMaxValue}
 						          onChangeMinValue={onChangeMinValue} />
 					) : (
-						<IncBlock minValue={minValue} maxValue={maxValue} score={score} incScore={incScore}
+						<IncBlock minValue={minValue} maxValue={maxValue} score={counterValues.score} incScore={incScore}
 						          removeScore={removeScore} />
 					)
 				}
--- src/store/reducers/counterValueReducer.ts
+++ src/store/reducers/counterValueReducer.ts
@@ -2,21 +2,32 @@ type IncrementCounterAT = {
 	type: 'INCREMENT_COUNTER'
 }
 
-type ActionsType = IncrementCounterAT
-
-type CounterValueT = { value: number }
-
-const initialState: CounterValueT = {
-	value: 0
+type RemoveScoreCounterAT = {
+	type: 'REMOVE_SCORE_COUNTER'
+	minValue: number
 }
 
-export const counterValueReducer = (state: CounterValueT = initialState, action: ActionsType): CounterValueT => {
+type ActionsType = IncrementCounterAT | RemoveScoreCounterAT
+
+export type CounterValuesT = { score: number }
+
+const initialState: CounterValuesT = {
+	score: 0
+}
+
+export const counterValueReducer = (state: CounterValuesT = initialState, action: ActionsType): CounterValuesT => {
 	switch (action.type) {
 		case 'INCREMENT_COUNTER':
-			return { ...state, value: state.value + 1 }
+			return { ...state, score: state.score + 1 }
+		case 'REMOVE_SCORE_COUNTER':
+			return { ...state, score: action.minValue }
 		default:
 			return state
 	}
 }
 
 export const IncrementCounterAC = (): IncrementCounterAT => ({ type: 'INCREMENT_COUNTER' })
+export const RemoveScoreCounterAC = (minValue: number): RemoveScoreCounterAT => ({
+	type: 'REMOVE_SCORE_COUNTER',
+	minValue
+})
