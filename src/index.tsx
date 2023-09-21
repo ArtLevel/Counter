335d713c4f9cf1817af3ac26c486b92eed18768c
ArtLevel <algitIT@rambler.ru>
2023-09-21 12:04:43 +0300

    Added Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 )
 root.render(
-	<App />
+	<Provider store={store}>
+		<App />
+	</Provider>
 )
 
